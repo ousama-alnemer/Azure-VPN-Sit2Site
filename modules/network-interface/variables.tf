45c8482b11fe9102b1df8bab85eb853df03af72b
Ousama Alnemer <ousama.alnemer@nordcloud.com>
2022-08-29 19:35:54 +0200

    Update variables.tf

--- modules/network-interface/variables.tf
+++ modules/network-interface/variables.tf
@@ -1,15 +1,15 @@
 
-variable "location" {
+variable "location-prd" {
   type        = string
   description = "The Azure Region where the Resource would be provisioned."
 }
 
-variable "resourcegroupname" {
+variable "resourcegroupname-prd" {
   type        = string
   description = "The Azure Resource Group Name."
 }
 
-variable "networkinterfacename" {
+variable "networkinterfacename-prd" {
   type        = string
   description = "The name of azure network interface card name."
 }
