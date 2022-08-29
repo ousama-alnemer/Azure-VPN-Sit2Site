6706cf03f1ec8d0db7e4f8c83a98f329900403e2
ousama.alnemer <ousama.alnemer@nordcloud.com>
2022-08-29 20:51:33 +0200

    ss

--- modules/vnet-prd/main.tf
+++ modules/vnet-prd/main.tf
@@ -1,40 +1,32 @@
-resource "azurerm_resource_group" "rg" {
-  name     = "vnet-prd-rg"
+resource "azurerm_resource_group" "example" {
+  name     = "example-resources"
   location = "West Europe"
 }
 
-resource "azurerm_virtual_network" "vnet" {
-  name                = "vnet-prd-01"
+resource "azurerm_network_security_group" "example" {
+  name                = "example-security-group"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+}
+
+resource "azurerm_virtual_network" "example" {
+  name                = "example-network"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
   address_space       = ["10.0.0.0/16"]
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-}
 
-resource "azurerm_subnet" "sub-01" {
-  name                 = "AzureFirewallSubnet"
-  resource_group_name  = azurerm_resource_group.rg.name
-  virtual_network_name = azurerm_virtual_network.vnet.name
-  address_prefixes     = ["10.0.1.0/24"]
-}
-
-resource "azurerm_public_ip" "ip" {
-  name                = "IP-prod-01"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-  allocation_method   = "Static"
-  sku                 = "Standard"
-}
-
-#resource "azurerm_firewall" "example" {
-#  name                = "testfirewall"
-#  location            = azurerm_resource_group.example.location
-#  resource_group_name = azurerm_resource_group.example.name
-#  sku_name            = "AZFW_VNet"
-#  sku_tier            = "Standard"
-
-  ip_configuration {
-    name                 = "configuration"
-    subnet_id            = azurerm_subnet.sub.id
-    public_ip_address_id = azurerm_public_ip.ip.id
+  subnet {
+    name           = "subnet1"
+    address_prefix = "10.0.1.0/24"
   }
- }
+
+  subnet {
+    name           = "subnet2"
+    address_prefix = "10.0.2.0/24"
+    security_group = azurerm_network_security_group.example.id
+  }
+
+  tags = {
+    environment = "Production"
+  }
+}
