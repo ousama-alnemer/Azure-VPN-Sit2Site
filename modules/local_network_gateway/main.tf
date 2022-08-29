38f1f40096c7825add5d478011af218e2dec154d
ousama.alnemer <ousama.alnemer@nordcloud.com>
2022-08-29 14:16:36 +0200

    qw

---
+++ modules/local_network_gateway/main.tf
@@ -0,0 +1,11 @@
+
+resource "azurerm_local_network_gateway" "lgw" {
+  name                = var._localnetworkgateway
+  resource_group_name = var.resourcegroupname
+  location            = var.location
+  gateway_address     = var.peergatewayaddress
+  address_space       = [var.peerIpaddress]
+}
+
+
+
---
+++ modules/local_network_gateway/output.tf
@@ -0,0 +1,5 @@
+output "_localnetworkgateway" {
+  description = "To return the name of the virtual network"
+  value       = azurerm_virtual_network.vnet.name
+  sensitive   = false
+}
