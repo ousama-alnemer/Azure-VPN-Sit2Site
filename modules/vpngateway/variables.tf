e4a2287f187d9eef1ceef418362e11f5ed955671
ousama.alnemer <ousama.alnemer@nordcloud.com>
2022-08-31 15:39:49 +0200

    dd

--- modules/vpngateway/variables.tf
+++ modules/vpngateway/variables.tf
@@ -24,12 +24,12 @@ variable "skuvalue" {
   description = "The name of azure Virtual Network."
 }
 
-variable "privateip" {
-  type        = string
-  description = "The name of azure Virtual Network."
-}
+#variable "privateip" {
+#  type        = string
+#  description = "The name of azure Virtual Network."
+#}
 
-variable "publicipname" {
-  type        = string
-  description = "The name of azure Virtual Network."
-}
+#variable "publicipname" {
+#  type        = string
+#  description = "The name of azure Virtual Network."
+#}
