440dbc2c4a4be6db8c0b00e4d549771a8c222a9c
ousama.alnemer <ousama.alnemer@nordcloud.com>
2022-08-31 13:25:02 +0200

    dd

--- modules/subnetvpngateway/output.tf
+++ modules/subnetvpngateway/output.tf
@@ -1,5 +1,5 @@
 output "_subnetid" {
   description = "To return the id of the subnet"
-  value       = azurerm_subnet.subnetvpn.id
+  value       = azurerm_subnet.subnetvpngateway.id
   sensitive   = false
 }
