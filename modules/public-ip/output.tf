c44d8f2ed3598a7f8ee4e04c23fd363dd05eead4
ousama.alnemer <ousama.alnemer@nordcloud.com>
2022-09-01 12:12:10 +0200

    s

--- modules/public-ip/output.tf
+++ modules/public-ip/output.tf
@@ -1,5 +1,5 @@
 output "_publicipname" {
   description = "To return the id of the public ip address"
-  value       = azurerm_public_ip.publicipname.name
+  value       = azurerm_public_ip.ip_configuration.publicipname.name
   sensitive   = false
 }
