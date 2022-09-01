9f7c896a879b10139f8d21c1e0fb101c02dd7a0f
ousama.alnemer <ousama.alnemer@nordcloud.com>
2022-09-01 08:16:12 +0200

    s

--- main.tf
+++ main.tf
@@ -106,7 +106,7 @@ module "vpngateway" {
   resourcegroupname     = module.resourcegroup._resourcegroupname
   skuvalue              = var.skuvalue
   subnetvpn             = var.subnetnamevpn
-  publicipname          = module.publicip._publicipname
+  publicipname          = module.publicip._publicipname publicipname
  # publicipid            = module.razurerm_publicip.publicipid
 }
 
