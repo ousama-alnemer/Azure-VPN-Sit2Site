d7a903898e0cb9d02ee39a3f751e67c9ef924b3e
ousama.alnemer <ousama.alnemer@nordcloud.com>
2022-09-01 18:58:44 +0200

    ddd

--- modules/vpngateway/main.tf
+++ modules/vpngateway/main.tf
@@ -2,18 +2,19 @@ resource "azurerm_virtual_network_gateway" "vpngateway" {
   name                = var.vpngatewayname
   location            = var.location
   resource_group_name = var.resourcegroupname
-  sku                 = "var.skuvalue"
+  sku                 = "VpnGw1"
   type                = "vpn" 
  
+
   ip_configuration {
   name                  = var.publicipname
-  public_ip_address_id            = "/subscriptions/79156d6a-8aa3-4824-9534-684ced8aec23/resourceGroups/vnet-hub-rg/providers/Microsoft.Network/publicIPAddresses/vpn-public-ip"
-  subnet_id              = "9442b8d4-afca-4849-b9b2-9ba0d9d2260e"
+  #public_ip_address_id  = module.publicip._publicipid
+  #subnet_id             = module.subnetvpngateway._subnetvpn.id
   
    }
 }
   
-  
+
   
   
   
