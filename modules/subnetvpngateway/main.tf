258100a19fe4fddf605f7d2e161e5e7d3dd88ad4
ousama.alnemer <ousama.alnemer@nordcloud.com>
2022-09-01 14:15:52 +0200

    s

--- modules/subnetvpngateway/main.tf
+++ modules/subnetvpngateway/main.tf
@@ -18,7 +18,7 @@ resource "azurerm_subnet" "subnetvpn" {
   resource_group_name  = var.resourcegroupname
   virtual_network_name = var.vnetname
   address_prefixes     = [var.subnetaddressvpn]
-  subnetid             = var.subvpnid
+#  subnetid             = var.subvpnid
 }
   
 
