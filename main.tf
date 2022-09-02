f285760fc90d31fbaf23b18fe3d285d590ca0766
ousama.alnemer <ousama.alnemer@nordcloud.com>
2022-09-02 11:28:25 +0200

    x

--- main.tf
+++ main.tf
@@ -57,15 +57,15 @@ resource "azurerm_virtual_network" "vnet" {
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   address_space       = ["10.100.0.0/16"]
-
-  subnet {
-    name           = "sub-01"
-    address_prefix = "10.100.1.0/24"
-  }
-  tags = {
-    environment = "Production"
-  }
 }
+
+resource "azurerm_subnet" "sub-01" {
+  name                 = "sub-01"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.100.2.0/24"]
+}
+
 
 resource "azurerm_subnet" "subgateway" {
   name                 = "GatewaySubnet"
